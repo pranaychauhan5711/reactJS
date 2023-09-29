d2a8038560d41a4a19299af4cbf9c51075aab92b
Pranay Chauhan <77404924+pranaychauhan5711@users.noreply.github.com>
2023-09-29 21:24:17 +0530

    Create MenuApi.js

---
+++ MenuApi.js
@@ -0,0 +1,84 @@
+const Menu = [
+
+  {
+    id:1,
+    image:"image/maggi.jpg",
+    name:"maggi",
+    category: "breakfast",
+    price:"$25",
+    description:" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, autem similique consectetur dignissimos nostrum, aspernatur sed error nobis sequi vero, distinctio in blanditiis earum voluptates?"
+  },
+  {
+    id:2,
+    image:"image/allupakoida.jpg",
+    name:"allu pakoida",
+    category: "lunch",
+    price:"$35",
+    description:" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, autem similique consectetur dignissimos nostrum, aspernatur sed error nobis sequi vero, distinctio in blanditiis earum voluptates?"
+  },
+  {
+    id:3,
+    image:"image/corn.jpg",
+    name:"corn",
+    category:"breakfast",
+    price:"$10",
+    description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, autem similique consectetur dignissimos nostrum, aspernatur sed error nobis sequi vero, distinctio in blanditiis earum voluptates?"
+  },
+  {
+    id:4,
+    image: "image/chola.jpg",
+    name: "chola",
+    category: "lunch",
+    price: "$50",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum fugit placeat expedita, molestiae rem facere?"
+  },
+  {
+    id:5,
+    image: "image/pizza.jpg",
+    name: "pizza",
+    category: "evening",
+    price:"$80",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, iure accusamus omnis nostrum iste aliquid?"
+  },
+  {
+    id:6,
+    image: "image/nonvegthali.jpg",
+    name: "Non-veg Thali",
+    category: "dinner",
+    price: "$180",
+    description:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, ad? Totam velit dignissimos necessitatibus numquam!"
+  },
+  {
+    id:7,
+    image: "image/sweet.jpg",
+    name:"Sweets",
+    category:"dinner",
+    price:"$60",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos magnam unde optio iusto illo sequi."
+  },
+  {
+    id:8,
+    image:"image/rajmarice.jpg",
+    name: "Rajma Rice",
+    category:"lunch",
+    price:"$60",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. A reiciendis obcaecati perspiciatis. Eos, cumque sequi."
+  },
+  {
+    id:9,
+    image:"image/samosa.jpg",
+    name:"samosa",
+    category:"evening",
+    price:"$50",
+    description:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi accusamus beatae natus? Dignissimos, vero cupiditate."
+  },
+  {
+    id:10,
+    image:"image/maggi.jpg",
+    name:"patanjali atta Noodles",
+    category:"Made In INDIA",
+    price:"$1000",
+    description:"After Completing Restaurant Project We can Have Patanjali Atta Noodles",
+  },
+];
+export default Menu;
