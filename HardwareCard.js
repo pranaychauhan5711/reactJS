1f08b40946520fdc6000fcac597653e590c98c03
Pranay Chauhan <77404924+pranaychauhan5711@users.noreply.github.com>
2023-10-04 16:53:44 +0530

    Create HardwareCard.js

---
+++ HardwareCard.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import "./style.css";
+import HardwareMenu from "./HardwareMenuApi.js";
+
+const HardwareCard = () => {
+      const [menuData,setMenuData] = useState(HardwareMenu);  
+      return(
+        <section className='main-card--container'>
+        {
+          menuData.map((curElem) => {
+          return(
+            <div className="card-container">
+            <img src={curElem.img} alt="images"/>
+            <div className="card">
+                  <div className="card-body">
+                    <div className="card-circle">
+
+                    <div className="card-number">
+                      <h1>{curElem.id}</h1>
+                    </div>
+
+                    </div>
+                    <div className="card-author">
+                      <div className="card-title">
+                        {curElem.name}
+                      </div>
+                    </div>
+                    <div className="card-description">
+                      {curElem.description}
+                    </div>
+                    <div className="card-read">
+                      Read More
+                    </div>
+                  </div>
+                </div>
+              </div>
+          )
+            
+          })
+        }
+        </section>            
+          )
+}
+export default  HardwareCard;
