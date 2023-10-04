868dbcc13431516edff3f4efdef52a50f26dcc8c
Pranay Chauhan <77404924+pranaychauhan5711@users.noreply.github.com>
2023-10-04 16:42:41 +0530

    Create HardwareMenuApi.js

---
+++ HardwareMenuApi.js
@@ -0,0 +1,33 @@
+const HardwareMenu = [
+  {
+    id:1,
+    image:"hardwareimage/screw.webp",
+    category:"screws",
+    name:"drillingscrew",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae optio nobis voluptatum similique temporibus.",
+    price:25
+  },
+  {
+    id:2,
+    image:"hardwareimage/nut.jpg",
+    category:"nuts",
+    name:"Nut",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae optio nobis voluptatum similique temporibus.",
+    price:25
+  },  {
+    id:3,
+    image:"hardwareimage/bolt.jpg",
+    category:"bolts",
+    name:"Bolts",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae optio nobis voluptatum similique temporibus.",
+    price:25
+  },  {
+    id:4,
+    image:"hardwareimage/kabje.jpg",
+    category:"Hinge",
+    name:"Hinge",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae optio nobis voluptatum similique temporibus.",
+    price:25
+  },
+];
+export default HardwareMenu;
