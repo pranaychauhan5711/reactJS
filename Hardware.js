3622c07ea7a384a226c5589039e32ab0ce6974da
Pranay Chauhan <77404924+pranaychauhan5711@users.noreply.github.com>
2023-10-05 16:49:05 +0530

    Update Hardware.js

--- Hardware.js
+++ Hardware.js
@@ -1,28 +1,29 @@
 import React,{useState} from 'react';
-import HardwareMenu from './HardwareMenuApi.js';
-import HardwareCard from './HardwareCard.js';
-import HardwareNavbar from './HardwareNavbar.js';
+import HardwareMenu from './hardwareMenuApi.js';
+import HardwareCard from './hardwareCard.js';
+import HardwareNavbar from './hardwareNavbar.js';
 import './style.css';
 const uniqueList = [
       ...new Set (
           HardwareMenu.map((curElem) => {
           return curElem.category;
           }))
-          ,"All";
+          ,"All"
 ];
-const filterItem = (category) => {
-          if(curElem === "All") {
-            setMenuData(HardwareMenu);
-            return;
-          }
-      const updatedList = HardwareMenu.filter ((curElem) => {
-        return curElem.category === category;
-      });
-      setmenuData(updatedList);
-};
+
 const Hardware = () => {
-  const [menuData,setMenuData] = useState(HardwareMenu); 
+  const [menuData,setMenuData] = useState(HardwareMenu);
   const [HardwareMenuData,setHardwareMenuData] = useState(uniqueList);
+  const filterItem = (category) => {
+    if(category === "All") {
+      setMenuData(HardwareMenu);
+      return;
+    }
+const updatedList = HardwareMenu.filter ((curElem) => {
+  return curElem.category === category;
+});
+setMenuData(updatedList);
+};
   return(
     <>
     <HardwareNavbar HardwareMenuData = {HardwareMenuData} filterItem = {filterItem}/>
